2852a5e4fe386b6eab3863910ea5d17faf6725a9
Sam Harvey <sam.harvey@bigant.com>
2025-11-25 10:15:08 +1100

    Edit the ranges

--- netlify/functions/getData.js
+++ netlify/functions/getData.js
@@ -2,12 +2,12 @@ export async function handler() {
   try {
     const url =
       "https://sheets.googleapis.com/v4/spreadsheets/1eAhYqy0og9IEGeijDHTxvCnpQN8MD1v1FmE1TTDNGuk/values:batchGet" +
-      "?ranges=Players" +
-      "&ranges=Matches" +
-      "&ranges=Finals" +
-      "&ranges=Schedule" +
-      "&ranges=SeasonElo" +
-      "&ranges=AllTimeElo" +
+      "?ranges=Players!A1:J19" + //players
+      "&ranges=Players!M1:V80" + //matches
+      "&ranges=Players!A22:J28" + //finals
+      "&ranges=Players!B29:C30" + //checkSchedule
+      "&ranges=Players!A79:B100" + //seasonElo
+      "&ranges=Players!E79:F115" + //allTimeElo
       "&key=" + process.env.GOOGLE_API_KEY;
 
     const res = await fetch(url);
