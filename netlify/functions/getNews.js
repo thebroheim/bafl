2b7f6e19bacb22a18bb26f0e62fa6564642eb1c3
Sam Harvey <sam.harvey@bigant.com>
2026-01-16 11:56:27 +1100

    Enable articles from sheets

---
+++ netlify/functions/getNews.js
@@ -0,0 +1,22 @@
+export async function handler() {
+  try {
+    const url =
+      "https://sheets.googleapis.com/v4/spreadsheets/1eAhYqy0og9IEGeijDHTxvCnpQN8MD1v1FmE1TTDNGuk/values:batchGet" +
+      "?ranges=BAFLTimes!A2:F200" + //news
+      "&key=" + process.env.GOOGLE_API_KEY;
+
+    const res = await fetch(url);
+    const data = await res.json();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data)
+    };
+
+  } catch (err) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: err.message })
+    };
+  }
+}
