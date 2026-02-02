53aecd0dd622abdb0a6d2e2ed58157b2288283f3
Sam Harvey <sam.harvey@bigant.com>
2026-02-02 16:31:27 +1100

    Create filter matches function

--- src/matchStats.js
+++ src/matchStats.js
@@ -30,7 +30,7 @@ function convertToObjects(values) {
 
     function filterMatches(matches){
         const filteredMatches = matches.filter(m => {
-        const seasonMatch = m.season === "All" ? true : m.season === Number(season);
+        const seasonMatch = m.season === "All" ? true : m.season === Number(m.season);
         const notForfeit = m.context !== 'forfeit';
         const miscCheck = m.context !== 'misc';
         
