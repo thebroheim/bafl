5aae8f834c5a09e4851ae353b5fe1f287b83d8f8
Sam Harvey <sam.harvey@bigant.com>
2026-05-04 09:32:39 +1000

    Logging

--- src/schedule.js
+++ src/schedule.js
@@ -184,8 +184,7 @@ function displayTable(players, container, tableDiv){
 
   let groupPrefix = showToggle[0].groupprefix
   // div1header.innerHTML = `${groupPrefix} 1`
-
-
+  console.log(players)
 
   if(players.length == 0){
     console.log('Do we get here?')
